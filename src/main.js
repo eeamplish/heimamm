f2016547a94be43c7a9bc739b0dcf720ee6e6af1
55open <123@qq.com>
2019-11-19 15:39:24 +0800

    登录模块-路由整合

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import router from './router'
+
+// 导入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 导入路由
+import router from './router/router'
+
+// 使用eleme-ui
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
--- src/router/router.js
+++ src/router/router.js
@@ -1,14 +1,18 @@
 // 导入Vue
 import Vue from 'vue';
 // 导入路由
-import router from 'vue-router';
 import VueRouter from 'vue-router';
+
+// 导入登录组件
+import login from '../views/login/login.vue'
 
 // 注册路由
 Vue.use(VueRouter);
 
 // 创建路由规则
-const routes = [];
+const routes = [
+  {path:"/login",component:login }
+];
 
 // 创建路由对象
 const router = new VueRouter({
